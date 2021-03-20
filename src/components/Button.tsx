47ca127dd25767f59c4895824504bada8febcacc
Lody Borgers <hi@lodybo.nl>
2021-03-20 21:34:11 +0100

    Some enhancements, and started the Button!

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,35 @@
 import React, { ReactNode, VoidFunctionComponent } from 'react';
+import classnames from 'classnames';
 
 export type Props = {
+  primary?: boolean;
+  onClick: () => void;
   children: ReactNode,
 };
 
-const Button: VoidFunctionComponent<Props> = ({ children }) => (
-  <button className="bg-primary-50">
+export const Button: VoidFunctionComponent<Props> = ({
+  primary = false,
+  onClick,
+  children
+}) => (
+  <button className={classnames(
+    'border-4',
+    'transition',
+    'px-5',
+    'py-2.5',
+    'cursor-pointer',
+    {
+      'border-primary': primary,
+      'bg-primary-light': primary,
+      'hover:bg-primary': primary,
+      'hover:border-primary-dark': primary,
+      'border-primary-light': !primary,
+      'hover:border-primary': !primary,
+      'hover:bg-primary-light': !primary,
+    }
+  )}
+    onClick={onClick}
+  >
     {children}
   </button>
 );
-
-export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,47 +6,47 @@ module.exports = {
   theme: {
     colors: {
       primary: {
-        light: 'D5E2EC',
-        DEFAULT: 'BCD0E0',
-        dark: 'B9CEDF',
+        light: '#EEF9FB',
+        DEFAULT: '#DCF2F7',
+        dark: '#CDEDF4',
       },
       secondary: {
-        light: 'EEF9FB',
-        DEFAULT: 'DCF2F7',
-        dark: 'CDEDF4',
+        light: '#D5E2EC',
+        DEFAULT: '#BCD0E0',
+        dark: '#ABC4D8',
       },
       tertiary: {
-        light: '725C5A',
-        DEFAULT: '655250',
-        dark: '5B4A48',
+        light: '#725C5A',
+        DEFAULT: '#655250',
+        dark: '#5B4A48',
       },
 
       white: {
-        light: 'FFFFFF',
-        DEFAULT: 'F2F5F8',
-        dark: 'E4EBF1',
+        light: '#FFFFFF',
+        DEFAULT: '#F2F5F8',
+        dark: '#E4EBF1',
       },
 
       black: {
-        light: '1D2935',
-        DEFAULT: '131B23',
-        dark: '0E141B',
+        light: '#1D2935',
+        DEFAULT: '#131B23',
+        dark: '#0E141B',
       },
 
       success: {
-        light: '9CBFAD',
-        DEFAULT: '90B6A3',
-        dark: '84AE99',
+        light: '#9CBFAD',
+        DEFAULT: '#90B6A3',
+        dark: '#84AE99',
       },
       warning: {
-        light: 'FADC89',
-        DEFAULT: 'F9D677',
-        dark: 'F8D062',
+        light: '#FADC89',
+        DEFAULT: '#F9D677',
+        dark: '#F8D062',
       },
       error: {
-        light: 'FB9E60',
-        DEFAULT: 'FA924C',
-        dark: 'FA8638',
+        light: '#FB9E60',
+        DEFAULT: '#FA924C',
+        dark: '#FA8638',
       },
     },
     fontFamily: {
