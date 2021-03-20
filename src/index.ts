03520712f6799f7c6d3ca0f3632adbc8e5d06e31
Lody Borgers <hi@lodybo.nl>
2021-03-20 15:42:57 +0100

    Extended and optimized a default Tailwind configuration for my theme.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import '@fontsource/bungee';
+import '@fontsource/bungee-shade';
+import '@fontsource/roboto-slab';
+import '@fontsource/roboto-slab/700.css';
+import '@fontsource/roboto-mono';
+
 import './styles/global.css';
 
 export { default as Button } from './components/Button';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,64 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   purge: [
     'src/**/*.tsx'
   ],
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     colors: {
-      primary: colors.teal
+      primary: {
+        light: 'D5E2EC',
+        DEFAULT: 'BCD0E0',
+        dark: 'B9CEDF',
+      },
+      secondary: {
+        light: 'EEF9FB',
+        DEFAULT: 'DCF2F7',
+        dark: 'CDEDF4',
+      },
+      tertiary: {
+        light: '725C5A',
+        DEFAULT: '655250',
+        dark: '5B4A48',
+      },
+
+      white: {
+        light: 'FFFFFF',
+        DEFAULT: 'F2F5F8',
+        dark: 'E4EBF1',
+      },
+
+      black: {
+        light: '1D2935',
+        DEFAULT: '131B23',
+        dark: '0E141B',
+      },
+
+      success: {
+        light: '9CBFAD',
+        DEFAULT: '90B6A3',
+        dark: '84AE99',
+      },
+      warning: {
+        light: 'FADC89',
+        DEFAULT: 'F9D677',
+        dark: 'F8D062',
+      },
+      error: {
+        light: 'FB9E60',
+        DEFAULT: 'FA924C',
+        dark: 'FA8638',
+      },
+    },
+    fontFamily: {
+      title: ['"Bungee Shade"', 'sans'],
+      heading: ['Bungee', 'sans'],
+      body: ['"Roboto Slab"', 'sans-serif'],
+      code: ['"Roboto Mono"', 'monospace'],
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
 }
