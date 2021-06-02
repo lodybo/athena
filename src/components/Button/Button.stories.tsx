407fe5b20ad9f93bbb6e4abe1db03d65bba47cfe
Lody Borgers <hi@lodybo.nl>
2021-06-02 22:07:55 +0200

    Finished Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Button, Props } from './Button';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faCheck,
+)
 
 export const ButtonTemplate = (props: Props) => <Button {...props}>Do not click me</Button>;
 
@@ -14,7 +20,22 @@ export const argTypes = {
       type: 'boolean'
     },
     table: {
-      defaultValue: false,
+      defaultValue: { summary: 'false'},
+    }
+  },
+  icon: {
+    name: 'Display an icon within the button.',
+    description: 'Passing an `icon` prop will display a Font Awesome icon within the button.',
+    control: {
+      type: 'text',
+    },
+  },
+  iconPlacement: {
+    name: 'Position of the icon.',
+    description: 'Determine at which side of the button the icon will be displayed.',
+    options: ['left', 'right'],
+    control: {
+      type: 'select',
     }
   },
   simple: {
@@ -24,18 +45,22 @@ export const argTypes = {
       type: 'boolean'
     },
     table: {
-      defaultValue: false,
-    }
+      defaultValue: { summary: 'false'},
+    },
   },
   theme: {
     name: 'Button theme',
     description: 'The theme the button is colored in',
     control: {
+      type: 'radio',
       options: [
         'primary',
         'secondary',
         'tertiary',
       ]
+    },
+    table: {
+      defaultValue: { summary: 'primary'},
     }
   }
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,16 @@
 import React, { ReactNode, VoidFunctionComponent } from 'react';
 import classnames from 'classnames';
 import { ClassValue } from "classnames/types";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export type Props = {
   classes?: ClassValue,
-  theme?: 'primary' | 'secondary' | 'tertiary',
-  solid?: boolean;
-  simple?: boolean;
+  theme: 'primary' | 'secondary' | 'tertiary',
+  solid: boolean;
+  simple: boolean;
+  icon?: IconProp;
+  iconPlacement: 'right' | 'left';
   onClick: () => void;
   children: ReactNode,
 };
@@ -16,12 +20,13 @@ export const Button: VoidFunctionComponent<Props> = ({
   solid = false,
   simple = false,
   theme = 'primary',
+  icon,
+  iconPlacement = 'left',
   onClick,
   children
 }) => (
   <button className={classnames(
     'transition',
-    'px-5',
     'py-2.5',
     'cursor-pointer',
     'focus:outline-none',
@@ -31,6 +36,9 @@ export const Button: VoidFunctionComponent<Props> = ({
     'focus-visible:ring-offset-primary',
     {
       'border-4': !solid,
+      'px-5': true,
+      'pl-2.5': icon && iconPlacement === 'left',
+      'pr-2.5': icon && iconPlacement === 'right',
       [`bg-${theme}-light`]: solid || !simple,
       [`hover:bg-${theme}`]: true,
       [`border-${theme}`]: !solid,
@@ -40,6 +48,26 @@ export const Button: VoidFunctionComponent<Props> = ({
   )}
     onClick={onClick}
   >
-    {children}
+    <span
+      className={classnames(
+        'flex',
+        'items-center',
+        {
+          'flex-row-reverse': iconPlacement === 'right',
+        }
+      )}
+    >
+      {icon && (
+        <FontAwesomeIcon
+          className={classnames({
+            'mr-2.5': icon && iconPlacement === 'left',
+            'ml-2.5': icon && iconPlacement === 'right',
+          })}
+          icon={icon}
+          fixedWidth
+        />
+      )}
+      {children}
+    </span>
   </button>
 );
